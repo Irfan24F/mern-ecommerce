fbc05d2aad3c1197c37a0991580bff888d267727
Irfan24F <ik0966072@gmail.com>
2024-11-15 10:25:41 +0530

    Update .babelrc.js

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
+
 module.exports = {
     presets: ["react-app"],
     plugins: [
       "@babel/plugin-proposal-private-property-in-object"  // or the alternative plugin
     ]
   };
-=======
 module.exports = {
     presets: ["react-app"],
     plugins: [
       "@babel/plugin-proposal-private-property-in-object"  // or the alternative plugin
     ]
   };
->>>>>>> origin/main
+ origin/main
   
