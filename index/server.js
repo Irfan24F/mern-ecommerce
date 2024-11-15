810c24d196f415f943d45c5ae3bf595e2cf79e2e
Irfan24F <ik0966072@gmail.com>
2024-11-15 15:26:33 +0530

    Update server.js

--- index/server.js
+++ index/server.js
@@ -10,14 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({
-    origin: [
-  
-        "https://ecommerce-bookstore-website-using-mern-stack-mb1d.vercel.app"
-    ],
-    methods: ["POST", "GET"],
-    credentials: true
-}));
+app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
