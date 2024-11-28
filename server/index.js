785ffdba396c64c9312cf159f4667617c297031c
Irfan24F <ik0966072@gmail.com>
2024-11-28 20:34:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin:["https://mern-ecommerce-1-ten.vercel.app"], 
+    origin:["https://mern-ecommerce-frontend-eight-blue.vercel.app
+.app"], 
     methods:["POST","GET"], 
     credentials:true
 }));
