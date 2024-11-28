3a513ece8a97e59624b70d04381a93d6703b44f7
Irfan24F <ik0966072@gmail.com>
2024-11-28 20:07:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"], 
+    methods:["POST","GET"], 
+    credentials:true
+}));
 
 // Static Files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
